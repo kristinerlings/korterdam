6cbee8dc13c25a0e96e61533336cd4f0432cc3f1
Lilou Barbier <lilou.barbier@student.howest.be>
2023-06-03 16:52:49 +0200

    css on deployment test

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,6 @@ export default ({ command }) => ({
         },
     },
     server: {
-        // respond to all network requests
         host: "0.0.0.0",
         strictPort: true,
         port: 3000,
@@ -33,4 +32,7 @@ export default ({ command }) => ({
         }),
         viteRestartValue,
     ],
+    css: {
+        extract: true, // Extract CSS into separate files during production build
+    },
 });
