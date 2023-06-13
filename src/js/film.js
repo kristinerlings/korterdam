d47e78f2b7a054fa9e2b8cc6bbeaf2510b3317b2
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-06-13 19:40:59 +0200

    carousel: not functioning properly

--- src/js/film.js
+++ src/js/film.js
@@ -9,65 +9,37 @@ const $btnPrev = document.querySelector('.prev');
 let counter = 0;
 
 const size = $card[0].clientWidth;
-/* $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-$btnNext.addEventListener('click', () => {
-  if (counter > $card.length - 3) return;
-  $carouselReel.style.transition = 'transform 0.4s ease-in-out';
-  counter++;
-  $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-});
-$btnPrev.addEventListener('click', () => {
-  if (counter <= 0) return;
-  $carouselReel.style.transition = 'transform 0.4s ease-in-out';
-  counter--;
-  $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-});
 
-//
-$carouselReel.addEventListener('transitionend', () => {
-  console.log($card[counter]);
-  if ($card[counter].id === 'lastslide') {
-    $carouselReel.style.transition = 'none';
-    counter = $card.length - 2;
-    $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-  }
-});
-$carouselReel.addEventListener('transitionend', () => {
-  console.log($card[counter]);
-  if ($card[counter].id === 'firstslide') {
-    $carouselReel.style.transition = 'none';
-    counter = $card.length - counter;
-    $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-  }
-}); */
 $carouselReel.style.transform = 'translateX(0)';
 
-$btnNext.addEventListener('click', () => {
-  if (counter < $card.length - 3) {
-    $carouselReel.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-  }
-});
+const init = () => {
+  $btnNext.addEventListener('click', () => {
+    if (counter < $card.length - 3) {
+      $carouselReel.style.transition = 'transform 0.4s ease-in-out';
+      counter++;
+      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
+    }
+  });
 
-$btnPrev.addEventListener('click', () => {
-  if (counter > 0) {
-    $carouselReel.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-  }
-});
+  $btnPrev.addEventListener('click', () => {
+    if (counter > 0) {
+      $carouselReel.style.transition = 'transform 0.4s ease-in-out';
+      counter--;
+      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
+    }
+  });
 
-$carouselReel.addEventListener('transitionend', () => {
-  if (counter === $card.length - 3) {
-    $carouselReel.style.transition = 'none';
-    counter = 0;
-    $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-  } else if (counter === -1) {
-    $carouselReel.style.transition = 'none';
-    counter = $card.length - 4;
-    $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-  }
-});
+  $carouselReel.addEventListener('transitionend', () => {
+    if (counter === $card.length - 3) {
+      $carouselReel.style.transition = 'none';
+      counter = 0;
+      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
+    } else if (counter === -1) {
+      $carouselReel.style.transition = 'none';
+      counter = $card.length - 4;
+      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
+    }
+  });
+};
 
-const init = () => {};
+init();
