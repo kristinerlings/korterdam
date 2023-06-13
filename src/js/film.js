fe76102d5e90df3f5ba4c13aad989bf243889dea
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-06-13 23:58:47 +0200

    carousel using batch filter from twig. Not completed.

--- src/js/film.js
+++ src/js/film.js
@@ -1,45 +1,39 @@
 console.log('film js works');
 
-//Source: https://www.codewithrandom.com/2023/02/12/simple-javascript-carousel-how-to-create-a-carousel-using-css-js/?expand_article=1
-
-const $carouselReel = document.querySelector('.carousel__reel');
-const $card = document.querySelectorAll('.card');
 const $btnNext = document.querySelector('.next');
 const $btnPrev = document.querySelector('.prev');
-let counter = 0;
 
-const size = $card[0].clientWidth;
+const showNextBatch = () => {
+  let $currentBatch = document.querySelector('.show');
+  $currentBatch.classList.remove('show');
 
-$carouselReel.style.transform = 'translateX(0)';
+  let $nextBatch = $currentBatch.nextElementSibling;
+  if ($nextBatch) {
+    $nextBatch.classList.add('show');
+    $btnPrev.disabled = false;
+  } else {
+    let $firstBatch = document.querySelector('.carousel__reel:first-child');
+    $firstBatch.classList.add('show');
+  }
+};
+
+const showPreviousBatch = () => {
+  let $currentBatch = document.querySelector('.show');
+  $currentBatch.classList.remove('show');
+
+  let $prevBatch = $currentBatch.previousElementSibling;
+  if ($prevBatch) {
+    $prevBatch.classList.add('show');
+  } else {
+    let $lastBatch = document.querySelector('.carousel__reel:last-child');
+
+    $lastBatch.classList.add('show');
+  }
+};
 
 const init = () => {
-  $btnNext.addEventListener('click', () => {
-    if (counter < $card.length - 3) {
-      $carouselReel.style.transition = 'transform 0.4s ease-in-out';
-      counter++;
-      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-    }
-  });
-
-  $btnPrev.addEventListener('click', () => {
-    if (counter > 0) {
-      $carouselReel.style.transition = 'transform 0.4s ease-in-out';
-      counter--;
-      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-    }
-  });
-
-  $carouselReel.addEventListener('transitionend', () => {
-    if (counter === $card.length - 3) {
-      $carouselReel.style.transition = 'none';
-      counter = 0;
-      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-    } else if (counter === -1) {
-      $carouselReel.style.transition = 'none';
-      counter = $card.length - 4;
-      $carouselReel.style.transform = 'translateX(' + -size * counter + 'px';
-    }
-  });
+  $btnNext.addEventListener('click', showNextBatch);
+  $btnPrev.addEventListener('click', showPreviousBatch);
 };
 
 init();
