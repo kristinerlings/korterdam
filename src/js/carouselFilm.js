68210c357cf436e1566a356df10824de268ecc60
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-06-18 00:43:16 +0200

    js second flickty carousel prep

--- src/js/carouselFilm.js
+++ src/js/carouselFilm.js
@@ -26,10 +26,36 @@ const initializeFlickityCarousel = () => {
   });
 };
 
+const generateCarouselHotspotCells = () => {
+  const $carouselContainer = document.querySelector('.container__hotspot');
+  const $carouselArticles = document.querySelectorAll('.hotspot__wrapper');
+
+  $carouselArticles.forEach((filmCardArticle) => {
+    const cell = document.createElement('div');
+    cell.classList.add('carousel-cell');
+    cell.appendChild(filmCardArticle);
+    $carouselContainer.appendChild(cell);
+  });
+};
+
+const initializeFlickityHotspotCarousel = () => {
+  new Flickity('.main-carousel', {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    groupCells: 1,
+    wrapAround: true,
+    pageDots: true,
+  });
+};
+
 const init = () => {
   document.addEventListener('DOMContentLoaded', () => {
     generateCarouselCells();
     initializeFlickityCarousel(); /*Always initialise it last! */
+
+    generateCarouselHotspotCells();
+    initializeFlickityHotspotCarousel();
   });
 };
 
