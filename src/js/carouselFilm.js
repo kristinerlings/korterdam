2c1743482cb3915c3b58bb9206c9a25df8e7df64
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-06-18 02:15:45 +0200

    js carousel hotspot working - still needs css adjustments + text

--- src/js/carouselFilm.js
+++ src/js/carouselFilm.js
@@ -22,40 +22,92 @@ const initializeFlickityCarousel = () => {
     contain: true,
     groupCells: 3,
     wrapAround: true,
-    pageDots: true,
+    pagedots: true,
   });
 };
 
-const generateCarouselHotspotCells = () => {
-  const $carouselContainer = document.querySelector('.container__hotspot');
-  const $carouselArticles = document.querySelectorAll('.hotspot__wrapper');
+const years = ['2021', '2022', '2023'];
 
-  $carouselArticles.forEach((filmCardArticle) => {
-    const cell = document.createElement('div');
-    cell.classList.add('carousel-cell');
-    cell.appendChild(filmCardArticle);
-    $carouselContainer.appendChild(cell);
+const titles = [
+  'Kortkijk x mental health',
+  'Another title',
+  'Yet another title',
+];
+
+const paragraphs = [
+  'Mental health is a seriouss issue. In Belgium alone, there are a lot of suicide deaths every year, because people are depressed. Kortrijk would like to raise awareness for this problem. That is why the theme of that Kortkijk film festival edition was mental health',
+  'Another paragraph.',
+  'Yet another paragraph.',
+];
+
+const paragraphsTwo = [
+  'By giving the youngsters the voice to speak out their thoughts about this subject, we can raise awareness for the people that are struggling. And we hope that because of this, people will become closer and help each other when they are struggling.',
+  'example two',
+  'example three',
+];
+
+const images = [
+  'public/1-hotspot-1.png',
+  'public/1-hotspot-1.svg',
+  'public/1-hotspot-1.svg',
+];
+
+/* const theme = ['theme', 'theme', 'theme'];
+const themes = ['theme', 'another theme', 'yet another theme']; */
+let currentIndex = 0; // Current index
+
+const replaceContent = () => {
+  // Get references to the elements
+  const $yearElement = document.querySelector('.hotspot__year');
+  const $titleElement = document.querySelector('.hotspot__title');
+  const $paragraphElement = document.querySelector('.hotspot__paragraph');
+  const $paragraphElementTwo = document.querySelector(
+    '.hotspot__paragraph--two'
+  );
+  const $imageElement = document.querySelector('.hotspot__img');
+  /*  const themeElement = document.querySelector('.hotspot__theme'); */
+
+  // Replace the content based on the current index
+  $yearElement.textContent = years[currentIndex];
+  /*   themeElement.textContent = themes[currentIndex]; */
+  $paragraphElementTwo.textContent = paragraphsTwo[currentIndex];
+  $titleElement.textContent = titles[currentIndex];
+  $paragraphElement.textContent = paragraphs[currentIndex];
+  $imageElement.src = images[currentIndex];
+
+  const $dots = document.querySelectorAll('.h--dot');
+  $dots.forEach((dot, index) => {
+    if (index === currentIndex) {
+      dot.classList.add('dot__active');
+    } else {
+      dot.classList.remove('dot__active');
+    }
   });
 };
 
-const initializeFlickityHotspotCarousel = () => {
-  new Flickity('.main-carousel', {
-    // options
-    cellAlign: 'center',
-    contain: true,
-    groupCells: 1,
-    wrapAround: true,
-    pageDots: true,
-  });
-};
+const $prevButton = document.querySelector('.hotspot__prev');
+$prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + years.length) % years.length; // Decrement index (with wrapping)
+  replaceContent();
+});
+
+const $nextButton = document.querySelector('.hotspot__next');
+$nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % years.length; // Increment index (with wrapping)
+  replaceContent();
+});
+
+// Initial content replacement
 
 const init = () => {
   document.addEventListener('DOMContentLoaded', () => {
     generateCarouselCells();
     initializeFlickityCarousel(); /*Always initialise it last! */
 
-    generateCarouselHotspotCells();
-    initializeFlickityHotspotCarousel();
+    replaceContent();
+
+    /*     generateCarouselHotspotCells();
+    initializeFlickityHotspotCarousel(); */
   });
 };
 
